59267931bb445b6ff5807276a1ecb3a407a926e3
roxasventus <magicwand10@naver.com>
2024-07-17 15:30:56 +0900

    [feat]: 근접무기(버그 해결), 원거리무기

--- Assets/Codes/Bullet.cs
+++ Assets/Codes/Bullet.cs
@@ -8,10 +8,40 @@ public class Bullet : MonoBehaviour
     public float damage;
     public int per;
 
-    public void Init(float damage, int per)
+    Rigidbody2D rigid;
+
+    public void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    public void Init(float damage, int per, Vector3 dir)
     {
         this.damage = damage;
         this.per = per;
+
+        // 관통이 -1(무한)보다 큰 것에 대해서는 속도 적용
+        if (per > -1)
+        {
+            // 속력을 곱해주어 총알이 날아가는 속도 증가시키기
+            rigid.velocity = dir * 15f;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collision.CompareTag("Enemy") || per == -1)
+        {
+            return;
+        }
+        // 관통 값이 하나씩 줄어들면서 -1이 되면 비활성화
+        per--;
+
+        if (per == -1)
+        {
+            rigid.velocity = Vector3.zero;
+            gameObject.SetActive(false);
+        }
     }
 
 }
