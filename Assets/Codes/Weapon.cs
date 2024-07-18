8099a86440875ba3e0de279a7fa914931aef8654
roxasventus <magicwand10@naver.com>
2024-07-18 15:15:51 +0900

    [feat]: 고정 타겟 원거리무기

--- Assets/Codes/Weapon.cs
+++ Assets/Codes/Weapon.cs
@@ -33,9 +33,11 @@ public class Weapon : MonoBehaviour
         // 무기 ID에 따라 로직을 분리
         switch (id)
         {
+            // 삽
             case 0:
                 transform.Rotate(Vector3.back * speed * Time.deltaTime); // Vector3.back이 시계방향
                 break;
+            // 자동조준
             case 1:
                 timer += Time.deltaTime;
 
@@ -47,6 +49,7 @@ public class Weapon : MonoBehaviour
                     Fire();
                 }
                 break;
+            // 바라보는 방향 조준
             case 2:
                 timer += Time.deltaTime;
 
@@ -58,7 +61,18 @@ public class Weapon : MonoBehaviour
                     Fire2();
                 }
                 break;
+            // 고정된 방향 조준
+            case 3:
+                timer += Time.deltaTime;
 
+                // speed 보다 커지면 초기화하면서 발사 로직 실행
+                // speed 값은 연사속도를 의미: 적을 수록 많이 발사
+                if (timer > speed)
+                {
+                    timer = 0f;
+                    Fire3();
+                }
+                break;
         }
 
         // .. Test Code ..
@@ -87,11 +101,16 @@ public class Weapon : MonoBehaviour
                 speed = 150; // 양수가 시계방향
                 Batch();
                 break;
-            // 총탄
+            // 자동조준
             case 1:
                 speed = 0.3f;
                 break;
+            // 바라보는 방향 조준
             case 2:
+                speed = 1f;
+                break;
+            // 고정된 방향 조준
+            case 3:
                 speed = 1f;
                 break;
         }
@@ -135,6 +154,7 @@ public class Weapon : MonoBehaviour
             bullet.GetComponent<Bullet>().Init(damage, -1, Vector3.zero); // bullet 컴포넌트 접근하여 속성 초기화 함수 호출, -1은 무한히 관통한다는 의미로 두었다
         }
     }
+
     // auto targeting
     void Fire()
     {
@@ -172,5 +192,54 @@ public class Weapon : MonoBehaviour
             bullet.GetComponent<Bullet>().Init(damage, count, dir);
         }
 
+    }
+    // Fixed direction targeting
+    void Fire3() {
+
+        for (int index = 0; index < count; index++)
+        {
+            
+            // 가져온 오브젝트의 Transform을 지역변수로 저장
+            Transform bullet ;
+
+            // 기존 오브젝트를 먼저 활용하고 모자란 것은 풀링에서 가져오기
+            if (index < transform.childCount)             // 자신의 자식 오브젝트 개수 확인은 childCount 속성에서
+            {
+                // index가 아직 childCount 범위 내라면 GetChild 함수로 가져오기
+                bullet = transform.GetChild(index);
+            }
+            else
+            {
+                bullet = GameManager.instance.pool.Get(prefabId).transform;
+                // 갓 생성된 탄환의 부모는 PoolManager이다. 갓 생성된 탄환은 플레이어를 따라가야 하므로 부모를 플레이어의 자식 오브젝트인 Weapon 0로 바꿔야 한다. 
+                bullet.parent = transform; // parent 속성을 통해 부모 변경
+            }
+            
+            bullet.GetComponent<Rigidbody2D>().velocity = transform.forward * speed;
+            bullet.position = transform.position;
+
+            // 탄환의 위치와 회전 초기화
+            bullet.localPosition = Vector3.zero;
+            bullet.localRotation = Quaternion.identity;
+
+            // 생성된 탄환을 적절한 위치에 배치
+            Vector3 rotVec = Vector3.forward * 360 * index / count;
+            bullet.Rotate(rotVec);
+            // Translate 함수로 자신의 위쪽으로 이동, 이동 방향은 Space.World 기준으로
+            bullet.Translate(bullet.up * 1.5f, Space.World);
+
+
+            // 배치가 완료되었으므로 더 이상 탄환이 플레이어를 따라갈 필요가 없다
+            bullet.parent = GameObject.Find("PoolManager").transform; // 다시 poolManager로 부모 변경
+
+
+
+            // 탄환이 바라보는 방향으로 발사
+            Quaternion.FromToRotation(Vector3.up, bullet.transform.up);
+            bullet.GetComponent<Bullet>().Init(damage, count, bullet.transform.up);
+
+
+        }
+        
     }
 }
