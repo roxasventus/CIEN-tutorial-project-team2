2dc6bdebf3394ca7323c82ed316c8bbbd626cdd8
SEONG WON CHOI <qwik0102@gmail.com>
2024-07-24 01:56:32 +0900

    [feat]방해물 투명화 기능 추가

---
+++ Assets/Codes/Transparent.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Rendering;
+using UnityEditor.UIElements;
+using UnityEngine;
+
+public class Transparnet : MonoBehaviour
+{
+    public float amount;
+
+    SpriteRenderer sprite;
+    Transform transform;
+    Player player;
+
+    private void Awake()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+        transform = GetComponent<Transform>();
+        player = GameManager.instance.player;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collision.CompareTag("Player"))
+            return;
+
+        if (player.transform.position.y < transform.position.y) //when player is in front of the pillar
+            return;
+
+        Color temp = sprite.color;
+        temp.a = amount;
+        sprite.color = temp;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (!collision.CompareTag("Player"))
+            return;
+
+        if(sprite.color.a < 1f)
+        {
+            Color temp = sprite.color;
+            temp.a = 1f;
+            sprite.color = temp;
+        }
+    }
+}
